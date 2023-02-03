3b58e12b10403c8b18a766224e9be76e1b31adcc
hungdangxung <120209106+hungdangxung@users.noreply.github.com>
2023-02-03 09:01:35 +0700

    Update Program.cs

--- Bai4_AVeryBigSum/Program.cs
+++ Bai4_AVeryBigSum/Program.cs
@@ -21,12 +21,12 @@ class Result
      */
     public static long aVeryBigSum(List<long> ar)
     {
-        long tong = 0;
+        long sum = 0;
         for (int i = 0; i < ar.Count; i++)
         {
-            tong += ar[i];
+            sum += ar[i];
         }
-        return tong;
+        return sum;
     }
 }
 class Solution
@@ -34,7 +34,6 @@ class Solution
     public static void Main(string[] args)
     {
         TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
         List<long> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt64(arTemp)).ToList();
         long result = Result.aVeryBigSum(ar);
         textWriter.WriteLine(result);
